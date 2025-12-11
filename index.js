866ff8c484af0283aed3622d10f8e3a6a3b76db6
Caio Lucas <caiolucasprogramador@gmail.com>
2025-12-11 17:03:11 -0300

    Adiciona middleware de error e parâmetro next nas rotas post, put e delete

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const pirates = require("./pirates.json")
 
 app.use(express.json())
 
-
 // MIDDLEWARES:
 function verificarPirata(req, res, next) {
     const { id } = req.params
@@ -64,38 +63,59 @@ app.get("/pirates/:id", verificarPirata, function(req, res) {
 })
 
 // ROTA POST
-app.post("/pirates", validarCampos, encontrarProximoId, function(req, res) {
+app.post("/pirates", validarCampos, encontrarProximoId, function(req, res, next) {
+    try {
+        res.newPirate.id = res.newId
+        pirates.push(res.newPirate)
 
-    res.newPirate.id = res.newId
-    pirates.push(res.newPirate)
+        fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
 
-    fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
-
-    console.log("Arquivo adicionado!")
-    res.status(201).json(res.newPirate);
+        console.log("Arquivo adicionado!")
+        res.status(201).json(res.newPirate);
+    } catch (erro) {
+        next(erro)
+    }
 })
 
 // ROTA PUT
-app.put("/pirates/:id", verificarPirata, indicePirata, function(req, res) {
-    const rePirate = req.body
+app.put("/pirates/:id", verificarPirata, indicePirata, function(req, res, next) {
+    try {
+        const rePirate = req.body
 
-    pirates[res.pirateIndex] = {...pirates[res.pirateIndex], ...rePirate, id: pirates[res.pirateIndex].id}
+        pirates[res.pirateIndex] = {...pirates[res.pirateIndex], ...rePirate, id: pirates[res.pirateIndex].id}
 
-    fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
+        fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
 
-    console.log("Arquivo alterado")
-    res.json(pirates[res.pirateIndex])
+        console.log("Arquivo alterado")
+        res.json(pirates[res.pirateIndex])
+    }
+    catch (erro) {
+        next(erro)
+    }
 })
 
 // ROTA DELETE
-app.delete("/pirates/:id", verificarPirata, indicePirata, function(req, res) {
-    
-    const deletedPirated = pirates.splice(res.pirateIndex, 1)
-    
-    fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
+app.delete("/pirates/:id", verificarPirata, indicePirata, function(req, res, next) {
+    try {    
+        const deletedPirated = pirates.splice(res.pirateIndex, 1)
+        
+        fs.writeFileSync('./pirates.json', JSON.stringify(pirates, null, 2))
 
-    res.status(200).json({
-        message: "Pirata removido com sucesso",
-        pirate: deletedPirated[0]
+        res.status(200).json({
+            message: "Pirata removido com sucesso",
+            pirate: deletedPirated[0]
+        })
+    }
+    catch (erro) {
+        next(erro)
+    }
+})
+
+// Middlewares de erro.
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(err.status || 500).json ({
+        mensagem: err.message || 'Erro interno do servidor',
+        erro: process.env.NODE_ENV === 'development' ? err.stack : undefined
     })
 })
